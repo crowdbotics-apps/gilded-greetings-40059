d0ef39a7fd2a10c26425c12049d2ea8362f2b42a
Crowdbotics <team@crowdbotics.com>
2023-04-20 19:45:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gilded-greetings-40059",
+  url: "https://gilded-greetings-40059.botics.co",
+  api: "https://gilded-greetings-40059.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
